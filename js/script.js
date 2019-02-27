b4fd97e3b8b004abe80eddbffee0309c5ac6ee6c
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-02-27 19:33:53 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+document.getElementById('uname').addEventListener("keyup", function() {
+	var username = document.forms["Form"]["uName"].value;
+	var err = document.getElementById('err');
+
+	if (username.length < 8) {
+		this.style.borderColor = "red";
+		err.innerHTML = "Username should be at least 8 characters"
+	} else {
+		this.style.borderColor = "green";
+		err.innerHTML = " "
+	}
+});
+
+document.getElementById('pword').addEventListener("keyup", function() {
+	var password = document.forms["Form"]["password"].value;
+	var err = document.getElementById('err');
+
+	if (password.length < 5) {
+		this.style.borderColor = "red";
+		err.innerHTML = "Password should be at least 5 characters"
+	} else {
+		this.style.borderColor = "green";
+		err.innerHTML = " "
+	}
+});
+
+document.getElementById('email').addEventListener('keyup', function() {
+	var email = document.forms["Form"]["email"].value;
+	if (email && /(^\w.*@\w+\.\w)/.test(email)) {
+		err.innerHTML = " "
+		this.style.borderColor = "green";
+	} else {
+		err.innerHTML = "Enter valid mail "
+		this.style.borderColor = "red";
+	}
+});
