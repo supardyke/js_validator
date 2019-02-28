104d2294d3a8b631e5e26af437ebb12b58af7301
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-02-28 01:46:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ document.getElementById('uname').addEventListener("keyup", function() {
 
 document.getElementById('pword').addEventListener("keyup", function() {
 	var password = document.forms["Form"]["password"].value;
-	var err = document.getElementById('err');
 
 	if (password.length < 5) {
 		this.style.borderColor = "red";
@@ -24,6 +23,18 @@ document.getElementById('pword').addEventListener("keyup", function() {
 	}
 });
 
+document.getElementById('cpword').addEventListener("keyup", function() {
+	var password = document.forms["Form"]["password"].value;
+	var cPassword = document.forms["Form"]["confirmPassword"].value;
+
+	if (cPassword != password || cPassword.length == 0) {
+		this.style.borderColor = "red";
+		err.innerHTML = "Passwords do no match"
+	} else {
+		this.style.borderColor = "green";
+		err.innerHTML = " "
+	}
+});
 document.getElementById('email').addEventListener('keyup', function() {
 	var email = document.forms["Form"]["email"].value;
 	if (email && /(^\w.*@\w+\.\w)/.test(email)) {
