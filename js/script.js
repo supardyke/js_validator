a248258c9954218942a50333bf21a544add926c9
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2019-03-05 05:10:59 +0100

    Sleek

--- js/script.js
+++ js/script.js
@@ -1,55 +1,55 @@
-document.getElementById('uname').addEventListener("keyup", function() {
-	var username = document.forms["Form"]["uName"].value;
-	var err = document.getElementById('err');
+	document.getElementById('uname').addEventListener("keyup", function() {
+		var username = document.forms["Form"]["uName"].value;
+		var err = document.getElementById('err');
 
-	if (username.length < 8) {
-		this.style.borderColor = "red";
-		err.innerHTML = "Username should be at least 8 characters"
-	} else {
-		this.style.borderColor = "green";
-		err.innerHTML = " "
-	}
-});
+		if (username.length < 8) {
+			this.style.borderColor = "red";
+			err.innerHTML = "Username should be at least 8 characters"
+		} else {
+			this.style.borderColor = "green";
+			err.innerHTML = " "
+		}
+	});
 
-document.getElementById('pword').addEventListener("keyup", function() {
-	var password = document.forms["Form"]["password"].value;
+	document.getElementById('pword').addEventListener("keyup", function() {
+		var password = document.forms["Form"]["password"].value;
 
-	if (password.length < 5) {
-		this.style.borderColor = "red";
-		err.innerHTML = "Password should be at least 5 characters"
-	} else {
-		this.style.borderColor = "green";
-		err.innerHTML = " "
-	}
-});
+		if (password.length < 5) {
+			this.style.borderColor = "red";
+			err.innerHTML = "Password should be at least 5 characters"
+		} else {
+			this.style.borderColor = "green";
+			err.innerHTML = " "
+		}
+	});
 
-document.getElementById('cpword').addEventListener("keyup", function() {
-	var password = document.forms["Form"]["password"].value;
-	var cPassword = document.forms["Form"]["confirmPassword"].value;
+	document.getElementById('cpword').addEventListener("keyup", function() {
+		var password = document.forms["Form"]["password"].value;
+		var cPassword = document.forms["Form"]["confirmPassword"].value;
 
-	if (cPassword != password || cPassword.length == 0) {
-		this.style.borderColor = "red";
-		err.innerHTML = "Passwords do no match"
-	} else {
-		this.style.borderColor = "green";
-		err.innerHTML = " "
-	}
-});
-document.getElementById('email').addEventListener('keyup', function() {
-	var email = document.forms["Form"]["email"].value;
-	if (email && /(^\w.*@\w+\.\w)/.test(email)) {
-		err.innerHTML = " "
-		this.style.borderColor = "green";
-	} else {
-		err.innerHTML = "Enter valid mail "
-		this.style.borderColor = "red";
-	}
-});
+		if (cPassword != password || cPassword.length == 0) {
+			this.style.borderColor = "red";
+			err.innerHTML = "Passwords do no match"
+		} else {
+			this.style.borderColor = "green";
+			err.innerHTML = " "
+		}
+	});
+	document.getElementById('email').addEventListener('keyup', function() {
+		var email = document.forms["Form"]["email"].value;
+		if (email && /(^\w.*@\w+\.\w)/.test(email)) {
+			err.innerHTML = " "
+			this.style.borderColor = "green";
+		} else {
+			err.innerHTML = "Enter valid mail "
+			this.style.borderColor = "red";
+		}
+	});
 
-var input = document.querySelectorAll("input[data-validator]");
-input.forEach(function(i){
-	var validateMethods = i.getAttribute("data-validator");
-	console.log(i);
-	console.log(validateMethods);
-	
-});
+	var input = document.querySelectorAll("input[data-validator]");
+	input.forEach(function(i) {
+		var validateMethods = i.getAttribute("data-validator");
+		console.log(i);
+		console.log(validateMethods);
+
+	});
